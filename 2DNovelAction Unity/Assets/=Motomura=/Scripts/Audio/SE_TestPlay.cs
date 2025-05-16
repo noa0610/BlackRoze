0428f75fba4c8d644e697663b2b13061eaef78af
本村煌雅 <human.kagoshima.kouga@gmail.com>
2025-05-16 13:34:43 +0900

    前回追加したスクリプトにコメントを追加　・　一部変更
    コメント追加
    　・「SaveLoadManager.cs」
    　・「AudioManager.cs」
    　・「SE_TestPlay.cs」
    変更点
    　・セーブ＆ロードをScripts階層に変更

--- 2DNovelAction Unity/Assets/=Motomura=/Scripts/Audio/SE_TestPlay.cs
+++ 2DNovelAction Unity/Assets/=Motomura=/Scripts/Audio/SE_TestPlay.cs
@@ -1,19 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
+//＝＝＝＝任意キーを押したらSEを流す機構＝＝＝＝
 
 public class SE_TestPlay : MonoBehaviour
 {
-    [SerializeField] private AudioClip _SoundSource;
-    [SerializeField] private AudioSource _AudioSource;
-    [SerializeField] KeyCode _Input;
+    [SerializeField] private AudioClip _SoundSource;//再生するSE
+    [SerializeField] private AudioSource _AudioSource;//再生するSEのコンポーネント
+    [SerializeField] KeyCode _Input;//任意のキー
 
 
-    void Update () 
+    void Update()
     {
         if (Input.GetKeyDown(_Input)) //指定したキーが押されたら
         {
-            Debug.Log ("SE再生");
+            Debug.Log("SE再生 : " + _SoundSource.name);//コンソールに表示
             _AudioSource.PlayOneShot(_SoundSource);//再生
         }
     }

9e30ec5086cd0a3a395c9cf613a3f0b5631ac37d
huna1379128479847 <human.kagoshima.hunagumayuuhi@gmail.com>
2025-07-01 00:57:54 +0900

    爆発弾

--- 2DNovelAction Unity/Assets/C#Scripts/Funaguma/Scripts/Unit/Bullet/Bullet.cs
+++ 2DNovelAction Unity/Assets/C#Scripts/Funaguma/Scripts/Unit/Bullet/Bullet.cs
@@ -5,9 +5,9 @@ namespace BlackRose
 {
     public class Bullet : MonoBehaviour, IStopableObject
     {
-        [SerializeField,Tooltip("常に衝突可能なレイヤー")] private LayerMask _canHitLayer;
-        private LayerMask _targetLayer;
-        private BulletStatus _status;
+        [SerializeField, Tooltip("常に衝突可能なレイヤー")] private LayerMask _canHitLayer;
+        protected LayerMask _targetLayer;
+        protected BulletStatus _status;
         public Transform Transform => transform;
 
         // 弾のステータス設定（生成時に呼ばれる想定）
@@ -28,19 +28,28 @@ namespace BlackRose
         {
             if (((1 << collision.gameObject.layer) & _canHitLayer) == 1)
             {
-                Destroy(gameObject);
+                Hitted_Another(collision);
             }
             if (((1 << collision.gameObject.layer) & _targetLayer) == 0)
                 return;
+            Hitted_Target(collision);
+        }
 
-            Debug.Log("Hit"); // ログ出力（当たった！）
+        // 目的じゃないオブジェクトにヒットした場合に呼ばれる
+        protected virtual void Hitted_Another(Collider2D collision)
+        {
+            Destroy(gameObject);
+        }
+        // 目的のオブジェクトにヒットした場合に呼ばれる
+        protected virtual void Hitted_Target(Collider2D collision)
+        {
             if (collision.transform.TryGetComponent<UnitBase>(out var target))
             {
+                Debug.Log("Hit to Target. Name : " + target.UnitStatusData.unitName); // ログ出力（当たった！）
                 UnitManager.instance.AddDamage(target, null, _status.damage);
             }
             Destroy(gameObject); // 弾を破壊（寿命）
         }
-
         // 以下、一時停止／再開など（未実装）
 
         public void Dispose()

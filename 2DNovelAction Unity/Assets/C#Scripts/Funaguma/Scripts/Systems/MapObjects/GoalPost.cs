cf9a37371eaa8f51af069a9de286511b8647e70b
huna1379128479847 <human.kagoshima.hunagumayuuhi@gmail.com>
2025-07-14 16:41:17 +0900

    シーンローダーとゴール機能実装

---
+++ 2DNovelAction Unity/Assets/C#Scripts/Funaguma/Scripts/Systems/MapObjects/GoalPost.cs
@@ -0,0 +1,29 @@
+﻿using Cysharp.Threading.Tasks;
+using HighElixir;
+using UnityEngine;
+
+namespace BlackRose
+{
+    [RequireComponent(typeof(BoxCollider2D))]
+    public class GoalPost : MonoBehaviour
+    {
+        [SerializeField] private float _waitTime = 3f;
+        [SerializeField] private string _toSceneName = "Title";
+        private SceneLoaderAsync _loader = new();
+        public bool _allowGoal = true;
+        public async UniTask Goal()
+        {
+            if (_allowGoal && !SceneLoaderAsync.IsLoading)
+            {
+                UnitManager.instance.Pause(true);
+                await UniTask.WaitForSeconds(_waitTime);
+                _loader.StartSceneLoad(_toSceneName);
+            }
+        }
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.tag.Equals("Player"))
+                _ = Goal();
+        }
+    }
+}

c8b21b4f1727ad31f83e05eb0f5c7ec6609e5d28
huna1379128479847 <human.kagoshima.hunagumayuuhi@gmail.com>
2025-07-18 23:26:00 +0900

    UI作成完了（未デバッグ）

---
+++ 2DNovelAction Unity/Assets/C#Scripts/Funaguma/Scripts/Systems/UI/HPUI.cs
@@ -0,0 +1,49 @@
+﻿using HighElixir.Pool;
+using HighElixir.Utilities;
+using System;
+using System.Collections.Generic;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BlackRose.UI
+{
+    public class HPUI : SingletonBehavior<HPUI>
+    {
+        [SerializeField] private Camera _camera;
+        [Header("Pool")]
+        [SerializeField] private Image _prefab;
+        [SerializeField] private RectTransform _containar;
+        [SerializeField] private int _size;
+        [Header("Data"), SerializeField] private Vector2 _delta;
+        private Pool<Image> _pool;
+        private Dictionary<object, (Image image, IDisposable disposable)> _owners = new();
+
+        public void Get(UnitBase owner)
+        {
+            if (_owners.ContainsKey(owner)) return;
+            var i = _pool.Get();
+            var amount = owner.StatusManager.GetStatusAmount(Status.HP);
+            var dis = i.UpdateAsObservable().Where(_ => i.isActiveAndEnabled).Subscribe(_ =>
+            {
+                Vector2 screenPos = _camera.WorldToScreenPoint(owner.transform.position);
+                i.rectTransform.localPosition = screenPos + _delta;
+                var ratio = amount.currentAmount / amount.ChangedMax;
+                i.fillAmount = ratio;
+            }).AddTo(this);
+            _owners[owner] = (i, dis);
+        }
+        public void Release(UnitBase owner)
+        {
+            _pool.Release(_owners[owner].image);
+            _owners[owner].disposable.Dispose();
+            _owners.Remove(owner);
+        }
+        protected override void Awake()
+        {
+            base.Awake();
+            _pool = new Pool<Image>(_prefab, _size, _containar, true);
+        }
+    }
+}

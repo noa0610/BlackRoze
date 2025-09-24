b0e372941ef7d1091b18f98ad8c5f54f58d199bd
huna1379128479847 <human.kagoshima.hunagumayuuhi@gmail.com>
2025-09-25 00:11:25 +0900

    Animators,Layer追加

---
+++ 2DNovelAction Unity/Assets/C#Scripts/Funaguma/Scripts/BlackRose/Core/Model/StateMachine/Base/Animators/AnimatorAnimationDriver.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace BlackRose.Core.Models.States.Animators
+{
+    public sealed class AnimatorAnimationDriver : IAnimationDriver
+    {
+        private readonly Animator _animator;
+        public string CurrentLayer { get; set; } = "Default";
+
+        public AnimatorAnimationDriver(Animator animator, string defaultLayer = "Default")
+        {
+            _animator = animator;
+            CurrentLayer = defaultLayer;
+        }
+
+        public void OnSetState(string stateKey)
+        {
+        }
+
+        public void OnTransition(string fromState, string toState, string animationTrigger)
+        {
+            if (HasTrigger(animationTrigger))
+            {
+                _animator.ResetTrigger(animationTrigger);
+                _animator.SetTrigger(animationTrigger);
+            }
+        }
+
+        private bool HasTrigger(string animeTrigger)
+        {
+            if (string.IsNullOrEmpty(animeTrigger))
+            {
+                return false;
+            }
+            return _animator
+                .parameters
+                .Any(p =>
+                p.type == AnimatorControllerParameterType.Trigger &&
+                p.name == animeTrigger);
+        }
+    }
+}

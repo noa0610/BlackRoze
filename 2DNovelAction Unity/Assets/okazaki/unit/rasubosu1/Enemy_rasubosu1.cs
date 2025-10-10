743b9c61c302f794f95770c2909acdf216c4a389
yozoratoru <human.kagoshima.okazaki@gmail.com>
2025-10-11 00:01:08 +0900

    [update]ラスボス１のステートの移行をとりあえずバグ出ないで終了

--- 2DNovelAction Unity/Assets/okazaki/unit/rasubosu1/Enemy_rasubosu1.cs
+++ 2DNovelAction Unity/Assets/okazaki/unit/rasubosu1/Enemy_rasubosu1.cs
@@ -1,14 +1,18 @@
-﻿using BlackRose.Core.Models.Helper;
+using BlackRose.Core.Models.Helper;
 using BlackRose.Core.Models.SearchSystems;
 using BlackRose.Core.Models.States;
 using UnityEngine;
 using HighElixir;
 using System.Collections.Generic;
 using BlackRose.Datas.Definitions;
+
+
+
 namespace BlackRose.Core.Models.Units
 {
     [RequireComponent(typeof(SearchAssistanceMono))]
-    public class Enemy_rasubosu1 : UnitBase
+    
+    public partial class Enemy_rasubosu1 : UnitBase
     {
         [SerializeField] private Rigidbody2D _rb;
         [SerializeField] private GameObject bulletPrefab;
@@ -16,110 +20,10 @@ namespace BlackRose.Core.Models.Units
         private static readonly Dictionary<States, string> _stateNames = EnumWrapper.GetDict<States>();
         [SerializeField] private BulletData _firewallBulletData; // 必要ならInspectorでセット
         [SerializeField] private LayerMask _firewallTargetLayer; // 必要ならInspectorでセット
-        [SerializeField] private Transform _firewallPoints; // 必要ならInspectorでセット
-        
-        public enum States
-        {
-            none,
-            idle,// 待機
-            dead,// 死亡
-            attackidle, // 攻撃待機
-            armpunch, // アームパンチ
-            diffusebeamgun, // 拡散ビーム砲
-            firewall, // ファイアウォール
-        }
-        private enum Triggers
-        {
-            None,
-            FoundPlayer,   // プレイヤーを発見した
-            Attackcooldown, // 攻撃クールダウンした
-            Attack1, // 攻撃１
-            Attack2, // 攻撃２
-            Attack3, // 攻撃３
-            Attack4, // 攻撃４
-            Attack1end, // 攻撃１した
-            Attack2end, // 攻撃２した
-            Attack3end, // 攻撃３した
-            shockwaveend, // ショックウェーブした
-            Event1, // イベント1が終わった
-            Playerdead, // プレイヤーが死亡
-            Died,          // 死亡した（HPが０になった）
-        }
-        protected override void RegisterStats()
-        {
-            // トランスミッショングループを作成
-            var idleTrigger = new[]                                // 待機ステートのトリガー
-            {
-                (Triggers.FoundPlayer, States.attackidle),              // イベント1が終わったで攻撃待機へ
-                (Triggers.Died, States.dead),                      // 死亡で死へ
-            };
-            var attackidleTrigger = new[]                          // 攻撃待機ステートのトリガー
-            {
-                (Triggers.Playerdead, States.idle),                // プレイヤーが死亡で待機へ
-                (Triggers.Died, States.dead),                      // 死亡で死へ
-                (Triggers.Attack1, States.armpunch),               // アームパンチでアームパンチへ
-                (Triggers.Attack2, States.diffusebeamgun),         // 拡散ビーム砲で拡散ビーム砲へ
-                (Triggers.Attack3, States.firewall),               // ファイアウォールでファイアウォールへ
-            };
-            var armpunchTrigger = new[]                            // アームパンチステートのトリガー        
-            {
-                (Triggers.Attack1end, States.attackidle),          // アームパンチ終了で攻撃待機へ         
-                (Triggers.Died, States.dead),                      // 死亡で死へ
-            };
-            var diffusebeamgunTrigger = new[]                      //拡散ビーム砲ステートのトリガー          
-            {
-                (Triggers.Attack2end, States.attackidle),          // 拡散ビーム砲終了で攻撃待機へ
-                (Triggers.Died, States.dead),                      // 死亡で死へ
-            };
-            var firewallTrigger = new[]                            // ファイアウォールステートのトリガー
-            {
-                (Triggers.Attack3end, States.attackidle),          // ファイアウォール終了で攻撃待機へ
-                (Triggers.Died, States.dead),                      // 死亡で死へ
-            };
-            // ステートマシンにStatesの移動先の追加
-            _stateMachine
-                .AddTransmissions(States.idle, idleTrigger)
-                .AddTransmissions(States.attackidle, attackidleTrigger)
-                .AddTransmissions(States.armpunch, armpunchTrigger)
-                .AddTransmissions(States.diffusebeamgun, diffusebeamgunTrigger)
-                .AddTransmissions(States.firewall, firewallTrigger);
-            // 待機
-            var idle = new Idle().SetAnimeTrigger("idle").SetCancelableProgress(0);
-            _stateMachine.AddState(States.idle, idle);
-            // 死亡
-            var died = new Idle().SetAnimeTrigger("died").SetCancelableProgress(0);
-            died.OnAnimationCompleted.AddListener(() =>
-            {
-                UnitManager.instance.RemoveUnit(this);
-                Destroy(gameObject);
-            });
-            _stateMachine.AddState(States.dead, died);
-            // 攻撃待機
-            var attackidle = new Idle_LazyEvent(3f).SetAnimeTrigger("attackidle").SetCancelableProgress(0);
-            attackidle.LazyEvent.AddListener(Attackjudgement);
-            _stateMachine.AddState(States.attackidle, attackidle);
-            // アームパンチ
-            var armpunch = new Idle().SetAnimeTrigger("idle").SetCancelableProgress(0);
-            _stateMachine.AddState(States.armpunch, armpunch);
-            // 拡散ビーム砲
-            var diffusebeamgun = new Idle().SetAnimeTrigger("idle").SetCancelableProgress(0);
-            _stateMachine.AddState(States.diffusebeamgun, diffusebeamgun);
-            // ファイアウォール
-            var firewall = new ShootForward(_firewallBulletData, _firewallTargetLayer)
-            .SetDirection(Vector2.right)
-            .SetMuzzle(_firewallPoints? _firewallPoints.gameObject : gameObject)
-            .SetAnimeTrigger("beamswordattack")
-            .SetCancelableProgress(0);
-            // 弾発射完了時にショックウェーブ終了トリガーを発火
-            firewall.onShootComplete.AddListener(() =>
-            {
-                _stateMachine.LazyChange(Triggers.Attack3end);
-            });
-            _stateMachine.AddState(States.firewall, firewall);
-        }
+        [SerializeField] private Transform[] _firewallPoints;// 必要ならInspectorでセット
+        [SerializeField] private FreeMove _freeMove;
+
         private SearchAssistanceMono _searchAssistance;
-
-
         private void SearchPlayer()
         {
             var list = UnitManager.instance.GetUnitList();
@@ -129,7 +33,6 @@ namespace BlackRose.Core.Models.Units
                 _stateMachine.ChangeState(Triggers.FoundPlayer);
             }
         }
-
         protected override void BeforeAwake()
         {
             _searchAssistance = GetComponent<SearchAssistanceMono>();
@@ -149,6 +52,8 @@ namespace BlackRose.Core.Models.Units
                     transform.localScale = scale;
                 }
             }
+            // beamswordattackステート中のみ判定
+
         }
         private bool IsMatchingState(States state)
         {
@@ -156,7 +61,7 @@ namespace BlackRose.Core.Models.Units
         }
         public void Attackjudgement()
         {
-            int attackIndex = Random.Range(0, 3); // 0〜3 の間でランダム
+            int attackIndex = UnityEngine.Random.Range(0, 3); // 0〜3 の間でランダム
 
             switch (attackIndex)
             {
@@ -190,5 +95,5 @@ namespace BlackRose.Core.Models.Units
         }
 
     }
-    
+
 }

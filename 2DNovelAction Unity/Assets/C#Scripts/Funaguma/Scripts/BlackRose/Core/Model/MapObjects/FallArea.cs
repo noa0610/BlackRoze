5de374ee6b394d8b183ee698fee0d965b59eb5fd
huna1379128479847 <human.kagoshima.hunagumayuuhi@gmail.com>
2025-11-21 23:16:15 +0900

    ステータス、プレイヤースポナー改善
    TODO:PlayerSpawnner.Respawn内コメントアウト解除

--- 2DNovelAction Unity/Assets/C#Scripts/Funaguma/Scripts/BlackRose/Core/Model/MapObjects/FallArea.cs
+++ 2DNovelAction Unity/Assets/C#Scripts/Funaguma/Scripts/BlackRose/Core/Model/MapObjects/FallArea.cs
@@ -19,7 +19,7 @@ namespace BlackRose.Core.Models.Objects
                 // プレイヤーが落下エリアに入ったときの処理
                 Debug.Log($"{collision.gameObject.name} has fallen into the area: {gameObject.name}");
                 // ここでプレイヤーをリスポーンさせるなどの処理を追加できます
-                PlayerSpawnner.Spawn(collision.GetComponent<UnitBase>());
+                PlayerSpawnner.instance.Spawn(collision.GetComponent<UnitBase>());
             }
         }
     }

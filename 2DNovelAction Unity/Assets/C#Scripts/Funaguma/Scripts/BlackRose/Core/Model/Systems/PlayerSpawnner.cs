31ffe3a9b00c78270795f3f4557e53de5efb0daf
huna1379128479847 <human.kagoshima.hunagumayuuhi@gmail.com>
2025-11-21 22:34:38 +0900

    リスポーン改善（PlayerSpawnnerをシーンに配置する必要がある）

---
+++ 2DNovelAction Unity/Assets/C#Scripts/Funaguma/Scripts/BlackRose/Core/Model/Systems/PlayerSpawnner.cs
@@ -0,0 +1,79 @@
+﻿using BlackRose.Core.Models.Units;
+using HighElixir;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace BlackRose.Core.Models.Objects
+{
+    public class PlayerSpawnner : SingletonBehavior<PlayerSpawnner>
+    {
+        [SerializeField] private RespawnPoint _currentRespawnPoint;
+        [SerializeField] private UnitBase player;
+        public void SetRespawnPoint(RespawnPoint respawnPoint)
+        {
+            _currentRespawnPoint = respawnPoint;
+        }
+        public void Spawn(UnitBase Player, RespawnPoint customRespawnPoint = null)
+        {
+            if (customRespawnPoint == null)
+            {
+                Player.transform.position = _currentRespawnPoint.transform.position;
+            }
+            else
+            {
+                Player.transform.position = customRespawnPoint.transform.position;
+            }
+        }
+
+        // ステータスをリフレッシュする
+        public void Respawn(UnitBase Player, RespawnPoint customRespawnPoint = null)
+        {
+            if (customRespawnPoint == null)
+            {
+                Player.transform.position = _currentRespawnPoint.transform.position;
+            }
+            else
+            {
+                Player.transform.position = customRespawnPoint.transform.position;
+            }
+            Player.Refresh();
+        }
+
+
+        public void InitialSpawn()
+        {
+            Spawn(player);
+            player.gameObject.SetActive(true);
+        }
+
+        public void SetTarget(UnitBase target)
+        {
+            player = target;
+        }
+
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            // In editor, keep _currentRespawnPoint synchronized to the RespawnPoint marked as start in the same scene
+            var all = FindObjectsOfType<RespawnPoint>(true);
+            foreach (var p in all)
+            {
+                if (!p.gameObject.scene.IsValid()) continue;
+                if (!p.gameObject.activeInHierarchy) continue;
+                if (p.IsStart)
+                {
+                    if (_currentRespawnPoint != p)
+                    {
+                        _currentRespawnPoint = p;
+                        EditorUtility.SetDirty(this);
+                    }
+                    return;
+                }
+            }
+        }
+#endif
+    }
+}
---
+++ 2DNovelAction Unity/Assets/C#Scripts/Funaguma/Scripts/BlackRose/Core/Model/Unit/Helper/RespawnBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using BlackRose.Core.Models.Objects;
+using UnityEngine;
+
+namespace BlackRose.Core.Models.Units.Helpers
+{
+    public sealed class RespawnBehaviour : MonoBehaviour
+    {
+        [SerializeField] private UnitBase unitBase;
+        public void Receive()
+        {
+            PlayerSpawnner.instance.Respawn(unitBase);
+        }
+    }
+}
